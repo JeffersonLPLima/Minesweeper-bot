a15494f208e78f2b9bb39168e08f571b756186f3
Jefferson Luiz <jefferson-sep@hotmail.com>
2014-12-24 20:37:20 -0300

    Adcionado imagens

---
+++ Minesweeper/Minesweeper/Forms/MinesweeperForm.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace Minesweeper
+{
+    partial class MinesweeperForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Panel panel2;
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Time = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelMatriz = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.jogoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.novoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ajudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tutorialToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sobreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fácilToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.médioToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.difícilToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            panel2 = new System.Windows.Forms.Panel();
+            panel2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            panel2.AutoSize = true;
+            panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            panel2.Controls.Add(this.label7);
+            panel2.Controls.Add(this.label6);
+            panel2.Controls.Add(this.label5);
+            panel2.Controls.Add(this.label4);
+            panel2.Controls.Add(this.label3);
+            panel2.Controls.Add(this.button1);
+            panel2.Controls.Add(this.label2);
+            panel2.Controls.Add(this.Time);
+            panel2.Controls.Add(this.label1);
+            panel2.Location = new System.Drawing.Point(12, 54);
+            panel2.Name = "panel2";
+            panel2.Size = new System.Drawing.Size(415, 101);
+            panel2.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(195, 63);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 17);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "PontosB";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(195, 31);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 17);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "PontosP";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(145, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "BOT";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(131, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Player";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(152, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Pontuação";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(9, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 74);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Bombas";
+            // 
+            // Time
+            // 
+            this.Time.Enabled = false;
+            this.Time.Location = new System.Drawing.Point(335, 20);
+            this.Time.Name = "Time";
+            this.Time.Size = new System.Drawing.Size(75, 74);
+            this.Time.TabIndex = 1;
+            this.Time.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(347, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tempo";
+            // 
+            // panelMatriz
+            // 
+            this.panelMatriz.AccessibleName = "painelTable";
+            this.panelMatriz.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panelMatriz.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelMatriz.Location = new System.Drawing.Point(12, 171);
+            this.panelMatriz.Name = "panelMatriz";
+            this.panelMatriz.Size = new System.Drawing.Size(415, 326);
+            this.panelMatriz.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.jogoToolStripMenuItem,
+            this.ajudaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(444, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // jogoToolStripMenuItem
+            // 
+            this.jogoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.novoToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.jogoToolStripMenuItem.Name = "jogoToolStripMenuItem";
+            this.jogoToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.jogoToolStripMenuItem.Text = "Jogo";
+            // 
+            // novoToolStripMenuItem
+            // 
+            this.novoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fácilToolStripMenuItem1,
+            this.médioToolStripMenuItem1,
+            this.difícilToolStripMenuItem1});
+            this.novoToolStripMenuItem.Name = "novoToolStripMenuItem";
+            this.novoToolStripMenuItem.Size = new System.Drawing.Size(175, 24);
+            this.novoToolStripMenuItem.Text = "Novo";
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(175, 24);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // ajudaToolStripMenuItem
+            // 
+            this.ajudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tutorialToolStripMenuItem,
+            this.sobreToolStripMenuItem});
+            this.ajudaToolStripMenuItem.Name = "ajudaToolStripMenuItem";
+            this.ajudaToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
+            this.ajudaToolStripMenuItem.Text = "Ajuda";
+            // 
+            // tutorialToolStripMenuItem
+            // 
+            this.tutorialToolStripMenuItem.Name = "tutorialToolStripMenuItem";
+            this.tutorialToolStripMenuItem.Size = new System.Drawing.Size(129, 24);
+            this.tutorialToolStripMenuItem.Text = "Tutorial";
+            // 
+            // sobreToolStripMenuItem
+            // 
+            this.sobreToolStripMenuItem.Name = "sobreToolStripMenuItem";
+            this.sobreToolStripMenuItem.Size = new System.Drawing.Size(129, 24);
+            this.sobreToolStripMenuItem.Text = "Sobre";
+            // 
+            // fácilToolStripMenuItem1
+            // 
+            this.fácilToolStripMenuItem1.Name = "fácilToolStripMenuItem1";
+            this.fácilToolStripMenuItem1.Size = new System.Drawing.Size(175, 24);
+            this.fácilToolStripMenuItem1.Text = "Fácil";
+            // 
+            // médioToolStripMenuItem1
+            // 
+            this.médioToolStripMenuItem1.Name = "médioToolStripMenuItem1";
+            this.médioToolStripMenuItem1.Size = new System.Drawing.Size(175, 24);
+            this.médioToolStripMenuItem1.Text = "Médio";
+            // 
+            // difícilToolStripMenuItem1
+            // 
+            this.difícilToolStripMenuItem1.Name = "difícilToolStripMenuItem1";
+            this.difícilToolStripMenuItem1.Size = new System.Drawing.Size(175, 24);
+            this.difícilToolStripMenuItem1.Text = "Difícil";
+            // 
+            // MinesweeperForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(444, 506);
+            this.Controls.Add(panel2);
+            this.Controls.Add(this.panelMatriz);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "MinesweeperForm";
+            this.Text = "MinesweeperForm";
+            this.Load += new System.EventHandler(this.MinesweeperForm_Load);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem jogoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem novoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ajudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tutorialToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sobreToolStripMenuItem;
+        public System.Windows.Forms.Panel panelMatriz;
+        private System.Windows.Forms.Button Time;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ToolStripMenuItem fácilToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem médioToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem difícilToolStripMenuItem1;
+    }
+}
---
+++ Minesweeper/Minesweeper/Forms/MinesweeperForm.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    public partial class MinesweeperForm : Form
+    {
+       /// <summary>
+       /// Image Locations
+       /// </summary>
+       private static String noDesmImg = "C:/Users/Jefferson Luiz/Desktop/Rep-Minesweeper/minesweeper-plp/Minesweeper/Minesweeper/Forms/Images/casaInativa.JPG";
+       private static String noMarcImg = "C:/Users/Jefferson Luiz/Desktop/Rep-Minesweeper/minesweeper-plp/Minesweeper/Minesweeper/Forms/Images/casaAtiva.JPG";
+       private static String Img1 = "C:/Users/Jefferson Luiz/Desktop/Rep-Minesweeper/minesweeper-plp/Minesweeper/Minesweeper/Forms/Images/1.JPG";
+       private static String Img2 = "C:/Users/Jefferson Luiz/Desktop/Rep-Minesweeper/minesweeper-plp/Minesweeper/Minesweeper/Forms/Images/2.JPG";
+       private static String Img3= "C:/Users/Jefferson Luiz/Desktop/Rep-Minesweeper/minesweeper-plp/Minesweeper/Minesweeper/Forms/Images/3.JPG";
+       private static String Img4 = "C:/Users/Jefferson Luiz/Desktop/Rep-Minesweeper/minesweeper-plp/Minesweeper/Minesweeper/Forms/Images/4.JPG";
+       private static String Img5 = "C:/Users/Jefferson Luiz/Desktop/Rep-Minesweeper/minesweeper-plp/Minesweeper/Minesweeper/Forms/Images/5.JPG";
+       private static String Img6 = "C:/Users/Jefferson Luiz/Desktop/Rep-Minesweeper/minesweeper-plp/Minesweeper/Minesweeper/Forms/Images/6.JPG";
+       private static String Img7 = "C:/Users/Jefferson Luiz/Desktop/Rep-Minesweeper/minesweeper-plp/Minesweeper/Minesweeper/Forms/Images/7.JPG";
+       private static String Img8 = "C:/Users/Jefferson Luiz/Desktop/Rep-Minesweeper/minesweeper-plp/Minesweeper/Minesweeper/Forms/Images/8.JPG";
+       private static String mineImg = "C:/Users/Jefferson Luiz/Desktop/Rep-Minesweeper/minesweeper-plp/Minesweeper/Minesweeper/Forms/Images/mina.jpg";
+       private static String flagImg = "C:/Users/Jefferson Luiz/Desktop/Rep-Minesweeper/minesweeper-plp/Minesweeper/Minesweeper/Forms/Images/flag.JPG";
+       
+        GameTable gameTable;
+        public MinesweeperForm()
+        {
+           
+            InitializeComponent();
+        }
+
+       
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja sair do jogo ?", "Minesweeper", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+
+        private void botao_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja sair do jogo ?", "Minesweeper", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void btnEvent_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(((Button)sender).Name); // imprime no Console o nome do botão clicado
+        }
+
+        private void MinesweeperForm_Load(object sender, EventArgs e)
+        {
+       //  int matrizX = gameTable.Rows;
+        // int matrizY = gameTable.Columns;
+          int  matrizX = 32;
+          int matrizY = 32;
+          Image img = Image.FromFile(noDesmImg);
+            panelMatriz.Size = new Size(25 * matrizX, 25 * matrizY);
+            panelMatriz.Visible = true;
+            Button[][] btn = new Button[matrizX][];
+            for (int x = 0; x < matrizX; x++)
+            {
+                btn[x] = new Button[matrizY];
+                for (int y = 0; y < matrizY; y++)
+                {
+                    btn[x][y] = new Button();
+                    btn[x][y].Name = Convert.ToString("casa" + x + "," + y);
+                    btn[x][y].AccessibleName = "casa";
+                    btn[x][y].Size = new Size(25, 25);
+                 //   .ImageLocation = @"C:\Users\Jefferson Luiz\Desktop\Rep-Minesweeper\minesweeper-plp\Minesweeper\Minesweeper\Forms\Images\casaInativa.JPG"; 
+
+
+                    btn[x][y].Image = img;
+                  //  btn[x][y].Text = Convert.ToString(x+","+y);
+                    btn[x][y].Visible = true;
+                    btn[x][y].Location = new Point(x * 25, y * 25);
+                    btn[x][y].Click += new EventHandler(this.btnEvent_Click);
+                    panelMatriz.Controls.Add(btn[x][y]);
+                    
+
+                }
+            }
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+
+
+
+            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MinesweeperForm());
+
+        }
+
+        
+     
+        
+    }
+}
